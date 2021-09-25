78f0d49649ad7df429e1727c57a2ac520e8eac41
eliablm <eliablopez@hotmail.com>
2021-09-24 20:52:46 -0500

    Agrega rutas

---
+++ src/components/EditarProducto.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function EditarProducto() {
+	return <h1>Editar productos</h1>;
+}
+
+export default EditarProducto;
---
+++ src/components/NuevoProducto.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NuevoProducto() {
+	return <h1> Nuevo producto</h1>;
+}
+
+export default NuevoProducto;
