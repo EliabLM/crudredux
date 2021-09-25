81049acd5c10318a26e1dcad5bb5e1c5e7907a72
eliablm <eliablopez@hotmail.com>
2021-09-24 21:01:18 -0500

    Agrega formulario para nuevos productos

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
 function NuevoProducto() {
-	return <h1> Nuevo producto</h1>;
+	return (
+		<div className="row justify-content-center">
+			<div className="col-md-8">
+				<div className="card">
+					<div className="card-body">
+						<h2 className="text-center mb-4 font-weight-bold">
+							Agregar Nuevo Producto
+						</h2>
+
+						<form>
+							<div className="form-group">
+								<label>Nombre Producto</label>
+								<input
+									type="text"
+									className="form-control"
+									placeholder="Nombre Producto"
+									name="nombre"
+								/>
+								<label>Precio Producto</label>
+								<input
+									type="number"
+									className="form-control"
+									placeholder="Precio Producto"
+									name="precio"
+								/>
+							</div>
+
+							<button className="btn btn-primary font-weight-bold text-uppercase d-block w-100">
+								Agregar
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default NuevoProducto;
