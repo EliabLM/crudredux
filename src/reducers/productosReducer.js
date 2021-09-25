b690b493ed50efdf08d530c04ed73e4728086a35
eliablm <eliablopez@hotmail.com>
2021-09-24 22:55:26 -0500

    Agrega configuración de REDUX e instala dependencias necesarias y crea el store

---
+++ src/reducers/productosReducer.js
@@ -0,0 +1,15 @@
+import { Switch } from 'react-router';
+
+// cada reducer tiene su propio state
+const initialState = {
+	productos: [],
+	error: null,
+	loading: false,
+};
+
+export default function (state = initialState, action) {
+	switch (action.type) {
+		default:
+			return state;
+	}
+}
